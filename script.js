706ed82a259e7dc7b4da22b0461936d3dade17ef
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-09-29 16:25:42 +0200

    2 nouvelles sections

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset; // Capture la position de défilement vertical
-    let windowHeight = window.innerHeight; // Récupère la hauteur de la fenêtre
-    let container = document.querySelector('main'); // Sélectionne le container principal
+    let scrollTop = window.pageYOffset;
+    let windowHeight = window.innerHeight;
+    let container = document.querySelector('main');
 
-    // Applique une translation horizontale proportionnelle au défilement vertical
     container.style.transform = `translateX(-${scrollTop / windowHeight * 100}vw)`;
 });
 
 // Fonction pour rediriger vers la section lors du clic sur un lien
 document.querySelectorAll('nav a').forEach(link => {
     link.addEventListener('click', function(e) {
-        e.preventDefault(); // Empêche le comportement par défaut
+        e.preventDefault();
 
-        const targetId = this.getAttribute('href').substring(1); // Récupère l'ID de la section
+        const targetId = this.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
-        
-        const sections = Array.from(document.querySelectorAll('main section'));
-        const index = sections.indexOf(targetSection); // Trouve l'index de la section cible
 
-        // Calcule la distance à traduire en fonction de l'index de la section
+        const sections = Array.from(document.querySelectorAll('main section'));
+        const index = sections.indexOf(targetSection);
+
         let container = document.querySelector('main');
-        container.style.transform = `translateX(-${index * 75}vw)`;
+        container.style.transform = `translateX(-${index * 100}vw)`;
     });
 });
