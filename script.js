9a9c7ea3ade8e46707b9f021c1143202cb08b439
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-09-29 15:52:26 +0200

    Commence à prendre forme

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ window.addEventListener('scroll', function() {
     // Applique une translation horizontale proportionnelle au défilement vertical
     container.style.transform = `translateX(-${scrollTop / windowHeight * 100}vw)`;
 });
+
+// Fonction pour rediriger vers la section lors du clic sur un lien
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // Empêche le comportement par défaut
+
+        const targetId = this.getAttribute('href').substring(1); // Récupère l'ID de la section
+        const targetSection = document.getElementById(targetId);
+        
+        const sections = Array.from(document.querySelectorAll('main section'));
+        const index = sections.indexOf(targetSection); // Trouve l'index de la section cible
+
+        // Calcule la distance à traduire en fonction de l'index de la section
+        let container = document.querySelector('main');
+        container.style.transform = `translateX(-${index * 75}vw)`;
+    });
+});
